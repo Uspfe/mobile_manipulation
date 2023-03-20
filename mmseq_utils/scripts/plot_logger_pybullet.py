081a88a27eb5e392088297fe36ba9c5d2585d97c
Tracy <xintong.du@robotics.utias.utoronto.ca>
2023-03-20 16:53:54 -0400

    add plotter for log data from sychronized pybullet simulation

---
+++ mmseq_utils/scripts/plot_logger_pybullet.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import argparse
+import os
+from mmseq_utils.logging import DataLogger, DataPlotter
+import matplotlib.pyplot as plt
+
+def plot_tracking(plotters):
+    ee_axes = plotters["control"].plot_ee_position()
+    base_axes = plotters["control"].plot_base_position()
+    plotters["sim"].plot_ee_position(ee_axes)
+    plotters["sim"].plot_base_position(base_axes)
+
+def construct_logger(path_to_folder):
+    path_to_npz = os.path.join(path_to_folder, "data.npz")
+    plotter = DataPlotter.from_npz(path_to_npz)
+
+    return plotter
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--folder", required=True, help="Path to data folder.")
+    parser.add_argument("--htmpc", action="store_true", help="plot htmpc info")
+    parser.add_argument("--tracking", action="store_true",
+                        help="plot tracking data")
+    parser.add_argument("--robot", action="store_true",
+                        help="plot tracking data")
+
+    parser.add_argument("--all", action="store_true",
+                        help="plot all")
+
+    args = parser.parse_args()
+    plotter = construct_logger(args.folder)
+    if args.all:
+        plotter.plot_all()
+    else:
+        if args.htmpc:
+            plotter.plot_mpc()
+        if args.tracking:
+            plotter.plot_tracking()
+        if args.robot:
+            plotter.plot_robot()
+
+    plt.show()
