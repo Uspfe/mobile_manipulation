7d625db18246f8cdb7d6371082a0218bf417dbab
Tracy <xintong.du@robotics.utias.utoronto.ca>
2023-07-09 14:48:42 -0400

    plot path comparison in base frame

--- mmseq_utils/scripts/plot_path_comparison.py
+++ mmseq_utils/scripts/plot_path_comparison.py
@@ -9,7 +9,7 @@ from mmseq_utils import math
 import matplotlib.pyplot as plt
 
 EE_POS_Target0 = np.array([1.5, -2.0])
-EE_POS_Target1 = np.array([4., 0.])     # home position + 4m in x direciton
+EE_POS_Target1 = np.array([3.5, -1.0])     # home position + 4m in x direciton
 Rc = 0.5 + 0.25         # robot radius + padding
 
 def closet_approaching_pose(curr_base_pos, curr_ee_pos, next_ee_pos, rc):
@@ -35,22 +35,40 @@ def path_comparison(folder_path):
     num_plotters = len(plotters)
     axes = None
     for id, p in enumerate(plotters):
-        axes = p.plot_base_path(axes, id)
+        axes = p.plot_base_path(axes, id, worldframe=False)    
 
-    plotters[0].plot_base_ref_path(axes, num_plotters, legend="")
-    plotters[0].plot_ee_waypoints(axes, num_plotters, legend="")
+    plotters[-1].plot_base_ref_path(axes, num_plotters, legend="", worldframe=False)
+    # plotters[0].plot_ee_waypoints(axes, num_plotters, legend="")
 
-    approach_pose = closet_approaching_pose(np.zeros(2), EE_POS_Target0, EE_POS_Target1, Rc)
-    print(approach_pose)
+    r_ew_w_ds = plotters[-1].data.get("r_ew_b_ds", [])
+    r_bw_w_ds = plotters[-1].data.get("r_bw_b_ds", [])
+    base_ref_path_text_pos = (r_bw_w_ds[0] + r_bw_w_ds[-1]) / 2
+    base_ref_path_text_pos += np.array([0, 0.02])
+    axes.text(base_ref_path_text_pos[0], base_ref_path_text_pos[1], "Base Reference Path", horizontalalignment='center')
+
+    axes.scatter(EE_POS_Target0[0],EE_POS_Target0[1],  edgecolor='b', facecolor='b')
+    axes.scatter(EE_POS_Target1[0],EE_POS_Target1[1], color='b')
+    axes.text(EE_POS_Target0[0], EE_POS_Target0[1]-0.2, "EE Waypoint #1", horizontalalignment='center')
+    axes.text(EE_POS_Target1[0], EE_POS_Target1[1]+0., "EE Waypoint #2", horizontalalignment='right')
+
+
+    approach_pose = closet_approaching_pose(np.zeros(2), EE_POS_Target0[:2], EE_POS_Target1[:2], Rc)
 
     arrow_length = 0.2
     axes.arrow(approach_pose[0], approach_pose[1],
                arrow_length*np.cos(approach_pose[2]), arrow_length*np.sin(approach_pose[2]), width=0.01, facecolor='r', edgecolor='r')
+    closet_approaching_pose_text_pos = approach_pose[:2] + np.array([np.cos(approach_pose[2]), np.sin(approach_pose[2])]) * arrow_length * 1.5
+    axes.text(closet_approaching_pose_text_pos[0], closet_approaching_pose_text_pos[1], "Intermediate\nBase Waypoint")
 
-    circle1 = plt.Circle(EE_POS_Target0, Rc, facecolor=[1,0,0,0.25], edgecolor='r')
+    circle1 = plt.Circle(r_ew_w_ds[0][:2], Rc, facecolor=[1,0,0,0.25], edgecolor='r')
     axes.add_patch(circle1)
+
+    circle_text_pos = EE_POS_Target0 + Rc * np.array([np.cos(2/3*np.pi), np.sin(2/3*np.pi) * 1.2])
+    axes.text(circle_text_pos[0], circle_text_pos[1], "Closest\nApproaching Circle", horizontalalignment="center")
+
     axes.set_aspect("equal")
     plt.legend()
+    plt.grid('on')
 
 
 
